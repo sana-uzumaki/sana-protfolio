0c8688cf46a60d13be5b8707f60343bb42e583ed
sanantheshwaran <sana0630c@gmail.com>
2025-09-20 21:02:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ for (var i = 0; i < links.length; i++) {
     }
   };
 }
+
+emailjs.init('ffrUFX5iYhWpk6xcr');
+    const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = ' __ejs-test-mail-service';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
